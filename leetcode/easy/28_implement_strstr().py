dd8f39c0ac0d4a6f5a7248f5954c31b3f26757b9
oneofsunshine <863958701@qq.com>
2018-02-04 11:43:27 +0800

    2018-2-24

---
+++ leetcode/easy/28_implement_strstr().py
@@ -0,0 +1,11 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            return haystack.find(needle)
+        else:
+            return -1
